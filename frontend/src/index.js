194757cdb5b5ef5a1d7eb18ee027228ff91eb199
Henrique <henriqueramalholima@gmail.com>
2023-07-28 21:12:41 -0300

    fix load html and css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,16 @@
 import { HomeController } from "./views/home/script/home_controller.js";
 
-class Index{
+class Index {
     constructor() {
         this.home = new HomeController();
     }
 
-    loadComponent(componentName, pathName) {
-        // Defina a pasta onde estão os componentes
-        var componentsPath = "../../../../src/components";
-      
-        // Carrega o HTML do componente e adiciona ao elemento com o ID fornecido
-        $("#" + componentName).load(componentsPath + "/" + pathName + "/html/" + componentName + ".html");
-      }
-      
-
-    initSistem(){
-        this.loadComponent('nav-bar', 'nav');
+    initSistem() {
         this.home.init();
     }
 }
-const index = new Index();
-index.initSistem();
+
+document.addEventListener("DOMContentLoaded", function () {
+    const index = new Index();
+    index.initSistem();
+});
--- frontend/src/views/home/script/home_controller.js
+++ frontend/src/views/home/script/home_controller.js
@@ -1,5 +1,19 @@
-export class HomeController{
+export class HomeController {
+    loadComponent(componentName, pathName) {
+        // Defina a pasta onde estão os componentes
+        var componentsPath = "../../../../src/components";
 
-    init(){
+        // Carrega o arquivo de estilo do componente de forma assíncrona usando a tag <link>
+        var link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = componentsPath + "/" + pathName + "/style/" + componentName + ".css";
+        document.head.appendChild(link);
+
+        // Carrega o HTML do componente e adiciona ao elemento com o ID fornecido
+        $("#" + componentName).load(componentsPath + "/" + pathName + "/html/" + componentName + ".html");
+    }
+
+    init() {
+        this.loadComponent('nav-bar', 'nav');
     }
 }
