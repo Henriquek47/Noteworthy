41f2cd89a0a8d3c113bda341f463a29e1408ef30
Henrique <henriqueramalholima@gmail.com>
2023-07-30 12:37:45 -0300

    create verifyAuth

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,53 +7,73 @@ class Index {
         this.loadNavBar('nav-bar', 'nav')
     }
 
-    async loadControllerForCurrentPage () {
+
+    authVerify(pageName) {
+        const isLoggin = false;
+        console.log(pageName !== 'categories' && pageName !== 'home' && isLoggin === false  )
+        if (pageName !== 'categories' && pageName !== 'home' && isLoggin === false) {
+            window.location.href = '/frontend/src/views/home/html/home.html';
+            return false;
+        }
+        return true;
+    }
+
+    async loadControllerForCurrentPage() {
         // Determina o nome da página baseado na URL
         let pageName = window.location.pathname.split('/').pop();
         // Remove a extensão .html se houver
         pageName = pageName.split('.').shift();
-    
-        console.log('name url: ' + pageName);
-    
-        // Carrega o controlador correspondente
-        await this.loadController(pageName);
+
+        const check = this.authVerify(pageName);
+        if(check){
+            await this.loadController(pageName);
+        }
+        
     }
-    
+
     async loadController(pageName) {
         // Nota: Estamos retornando a promessa aqui
         try {
             const module = await import(`./views/${pageName}/script/${pageName}_controller.js`);
             // Cria uma nova instância do controlador
             this[`${pageName}Controller`] = new module[`${pageName.charAt(0).toUpperCase() + pageName.slice(1)}Controller`]();
-            return this[`${pageName}Controller`];
+            return this[`${pageName}Controller`].init();
         } catch (err) {
             console.error(`Erro ao carregar o controlador ${pageName}: `, err);
         }
     }
-    
 
-    loadNavBar(componentName, pathName) {
+
+    async loadNavBar(componentName, pathName) {
         var componentsPath = "../../../components";
 
         var link = document.createElement("link");
         link.rel = "stylesheet";
         link.href = componentsPath + "/" + pathName + "/style/" + componentName + ".css";
         document.head.appendChild(link);
-        console.log(componentsPath + "/" + pathName + "/html/" + componentName + ".html")
-        $("#" + componentName).load(componentsPath + "/" + pathName + "/html/" + componentName + ".html", () => {
-            const dropdownLinks = document.querySelectorAll(".dropdown-link");
-            console.log(dropdownLinks);
-            if (dropdownLinks.length > 0) {
-                dropdownLinks.forEach(link => {
-                    link.addEventListener("click", function (event) {
-                        event.preventDefault(); // Evita que o link redWirecione
-                        console.log(dropdownLinks);
-                        toggleDropdown();
-                    });
-                });
-            }
+
+        await new Promise((resolve, reject) => {
+            $("#" + componentName).load(componentsPath + "/" + pathName + "/html/" + componentName + ".html", (response, status, xhr) => {
+                if (status == "error") {
+                    reject(xhr.status + ": " + xhr.statusText);
+                } else {
+                    resolve(response);
+                }
+            });
         });
+
+        const dropdownLinks = document.querySelectorAll(".dropdown-link");
+        if (dropdownLinks.length > 0) {
+            dropdownLinks.forEach(link => {
+                link.addEventListener("click", function (event) {
+                    event.preventDefault(); // Evita que o link redirecione
+                    console.log(dropdownLinks);
+                    toggleDropdown();
+                });
+            });
+        }
     }
+
 }
 
 document.addEventListener("DOMContentLoaded", () => {
